9c23d7782d16f054475be1d7b5560dfbc20a5fa1
harinath799 <harikopperla2004@gmail.com>
2023-10-30 12:16:04 +0530

    main

--- firstproject/src/pack1/welcome.java
+++ firstproject/src/pack1/welcome.java
@@ -6,7 +6,8 @@ public class welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("...............................");
-		System.out.println(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,");
+		System.out.println(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,");	
+		System.out.println("11111111111111111111111111111111111");
 		
 
 	}
