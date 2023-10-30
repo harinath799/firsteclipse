51c2dbfdaa5d7eed78bf050ed9c8bdadcf6c0631
harinath799 <harikopperla2004@gmail.com>
2023-10-30 11:58:16 +0530

    second commit

--- firstproject/src/pack1/welcome.java
+++ firstproject/src/pack1/welcome.java
@@ -6,6 +6,8 @@ public class welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("...............................");
+		System.out.println(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,");
+		
 
 	}
 
